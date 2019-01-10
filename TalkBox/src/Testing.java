732759e6a224a86eaf0e80b6a789472aca8f3aac
Dion <Dion@ChinaManDionTu>
2019-01-10 00:26:05 -0500

    just playing around now...

--- TalkBox/src/Testing.java
+++ TalkBox/src/Testing.java
@@ -5,6 +5,7 @@ public class Testing {
 		// TODO Auto-generated method stub
 		System.out.println("Fun with Git!");
 		System.out.println("More fun with the push!");
+		System.out.println("Now im just playing around...");
 	}
 
 }
