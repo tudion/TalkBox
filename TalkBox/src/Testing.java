bac40324a1f01a36d3c13c9bcb0abec59f81cba2
ashishrt <ashishrt@ea77.eecs.yorku.ca>
2019-01-14 12:12:10 -0500

    test 1

--- TalkBox/src/Testing.java
+++ TalkBox/src/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 		// TODO Auto-generated method stub
 		System.out.println("Fun with Git!");
 		System.out.println("More fun with the push!");
-		System.out.println("Now im just playing around...");
+		System.out.println("Now im just playing around...test 1");
 		System.out.println("Just another test");
 	}
 
