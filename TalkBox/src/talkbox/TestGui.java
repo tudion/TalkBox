fa392c6d50f9b46057e15511c66b73e3cbf48238
Dion <Dion@ChinaManDionTu>
2019-01-20 15:59:06 -0500

    Added 2 buttons w/ +function play audio WAV files from a local directory

--- TalkBox/src/talkbox/TestGui.java
+++ TalkBox/src/talkbox/TestGui.java
@@ -3,10 +3,20 @@ package talkbox;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
 
 public class TestGui {
 
-	private JFrame frame;
+	private JFrame frmTalkbox;
 
 	/**
 	 * Launch the application.
@@ -16,7 +26,7 @@ public class TestGui {
 			public void run() {
 				try {
 					TestGui window = new TestGui();
-					window.frame.setVisible(true);
+					window.frmTalkbox.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +45,53 @@ public class TestGui {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTalkbox = new JFrame();
+		frmTalkbox.setTitle("TalkBox V1.0");
+		frmTalkbox.setBounds(100, 100, 450, 300);
+		frmTalkbox.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frmTalkbox.getContentPane().add(panel, BorderLayout.NORTH);
+		
+		JButton btnAudio1 = new JButton("Audio 1");
+		btnAudio1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				try {
+					String filename = "C:/Users/Dion/Desktop/Voice_Recorder/voice_recording_2.wav";
+					// opens audio input stream
+					File soundFile = new File(filename);
+					AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+					// get a sound clip resource
+					Clip clip = AudioSystem.getClip();
+					clip.open(audioIn);
+					clip.start();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		panel.add(btnAudio1);
+		
+		JButton btnAudio2 = new JButton("Audio 2");
+		btnAudio2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				try {
+					String filename = "C:/Users/Dion/Desktop/Voice_Recorder/voice_recording_2.wav";
+					// opens audio input stream
+					File soundFile = new File(filename);
+					AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+					// get a sound clip resource
+					Clip clip = AudioSystem.getClip();
+					clip.open(audioIn);
+					clip.start();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		panel.add(btnAudio2);
 	}
 
 }
