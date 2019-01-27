7d001cd3853cb67abd352d4a795eb4107b45d670
Dion <tudion@tudion@my.yorku.ca>
2019-01-26 19:00:58 -0500

    Updated GUI for ConfigurationApp

--- TalkBox/src/talkbox/TalkBoxConfigurationApp.java
+++ TalkBox/src/talkbox/TalkBoxConfigurationApp.java
@@ -7,6 +7,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
+import javax.swing.JLayeredPane;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.Panel;
+import java.awt.Button;
+import javax.swing.JLabel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JFormattedTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class TalkBoxConfigurationApp extends JFrame {
 
@@ -37,7 +52,56 @@ public class TalkBoxConfigurationApp extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblAudioFolderName = new JLabel("Choose the Audio Files folder");
+		panel_1.add(lblAudioFolderName);
+		
+		JButton btnAudioFolder = new JButton("Upload");
+		panel_1.add(btnAudioFolder);
+		
+		Panel panel_2 = new Panel();
+		panel.add(panel_2, BorderLayout.EAST);
+		panel_2.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JTextArea txtrInstructions1 = new JTextArea();
+		txtrInstructions1.setBackground(SystemColor.control);
+		txtrInstructions1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		txtrInstructions1.setWrapStyleWord(true);
+		txtrInstructions1.setLineWrap(true);
+		txtrInstructions1.setText("To add a sentence, click \"Start Recording\" and ");
+		panel_2.add(txtrInstructions1);
+		
+		JTextArea txtrInstructions2 = new JTextArea();
+		txtrInstructions2.setBackground(SystemColor.control);
+		txtrInstructions2.setLineWrap(true);
+		txtrInstructions2.setWrapStyleWord(true);
+		txtrInstructions2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		txtrInstructions2.setText("when you're finished, Click \"Stop Recording\"");
+		panel_2.add(txtrInstructions2);
+		
+		JButton btnStartRecord = new JButton("Start Recording");
+		panel_2.add(btnStartRecord);
+		
+		JButton btnStopRecord = new JButton("Stop Recording");
+		panel_2.add(btnStopRecord);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3, BorderLayout.SOUTH);
+		
+		JButton btnConfigure = new JButton("Configure");
+		btnConfigure.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_3.add(btnConfigure);
 	}
-
 }
